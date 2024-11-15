658bbf16855b3d1548a66e91c30da8debe3a08be
Randy <randysr2803@gmail.com>
2024-11-15 21:35:30 +0100

    creando rutas y algunos componentes como el formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container } from "react-bootstrap"
+import { Routes, Route, Navigate } from "react-router-dom"
+import NewNote from "./components/NewNote"
 function App() {
   
+  return (
+    <Container className="my-4">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/new" element={<NewNote />} />
+        <Route path="/:id">
+          <Route index element={<h1>Show</h1>} />   
+          <Route path="edit" element={<h1>Edit</h1>} />   
+        </Route>
 
-  return <h1>Preparando app</h1>
+        {/* En caso de que la url es cualquier cosa menos alguna ruta se devulve a Home */}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Container>
+  )
   
 }
 
